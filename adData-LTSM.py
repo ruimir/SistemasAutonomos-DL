9f1fce061f7fb3056295fffd46b804f4710db16a
ruimir <a75488@alunos.uminho.pt>
2018-05-27 19:49:31 +0100

    tentativa (com muito mau sucesso) de ltsm

--- adData-LTSM.py
+++ adData-LTSM.py
@@ -9,60 +9,33 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 
 # fixar random seed para se puder reproduzir os resultados
+from sklearn.preprocessing import MinMaxScaler
+
 seed = 9
 np.random.seed(seed)
 
-# Etapa 1 - preparar o dataset
-'''
-fazer o download da sequencias do valor das ações da google GOOGL stock data (fonte yahoo.com)
-dataset:
-http://chart.finance.yahoo.com/table.csv?s=GOOGL&a=11&b=15&c=2011&d=29&e=10&f=2016&g=d&ignore=.csv
-A função get_stock_data é generica para ir buscar dados à yahoo.com
-trata-se uma tabela com: ['Date','Open','High','Low','Close','Volume','Adj Close']
-Vamos śo utilizar os campos ['Open','High','Close']
-'''
 
-
-def get_stock_data(stock_name, normalized=0, file_name=None):
-    if not file_name:
-        file_name = stock_name
-    col_names = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
-    stocks = pd.read_csv(file_name, header=0,
-                         names=col_names)  # fica numa especie de tabela exactamente como estava no csv (1350 linhas,7 colunas)
-    df = pd.DataFrame(stocks)  # neste caso não vai fazer nada
-    date_split = df['Date'].str.split('-').str  # não vai servir para nada
-    df['Year'], df['Month'], df['Day'] = date_split  # não vai servir para nada
-    df["Volume"] = df["Volume"] / 10000  # não vai servir para nada
-    df.drop(df.columns[[0, 3, 5, 6, 7, 8, 9]], axis=1, inplace=True)  # vou só ficar com as colunas 1,2,4
+# Carregar os dados do .csv
+def get_ad_data(normalized=0, file_name=None):
+    col_names = ['Month', 'Advertising', 'Sales']
+    activity = pd.read_csv(file_name, header=0)
+    df = pd.DataFrame(activity)  # Criar dataFrame
+    df.drop(df.columns[[0]], axis=1, inplace=True)  # Largar coluna com data
+    if normalized == 1:
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        dataset = scaler.fit_transform(df)
+        return dataset
     return df
 
 
-def load_GOOGL_stock_dataset():
-    stock_name = 'GOOGL'
-    return get_stock_data(stock_name, 0, 'table.csv')
-
-
-def pre_processar_GOOGL_stock_dataset(df):
-    df['High'] = df['High'] / 100
-    df['Open'] = df['Open'] / 100
-    df['Close'] = df['Close'] / 100
-    return df
-
-
-# Visualizar os top registos da tabela
-def visualize_GOOGL():
-    df = load_GOOGL_stock_dataset()
-    print('### Antes do pré-processamento ###')
-    print(df.head())  # mostra só os primeiros 5 registos
-    df = pre_processar_GOOGL_stock_dataset(df)
-    print('### Após o pré-processamento ###')
-    print(df.head())
-
-
+# Comments Prof:
 # função load_data do lstm.py configurada para aceitar qualquer número de parametros
 # o último atributo é que fica como label (resultado)
 # stock é um dataframe do pandas (uma especie de dicionario + matriz)
 # seq_len é o tamanho da janela a ser utilizada na serie temporal
+#
+# Comments grupo:
+# Função alterada para o problema (separação esntre dados de treino e teste)
 def load_data(df_dados, janela):
     qt_atributos = len(df_dados.columns)
     mat_dados = df_dados.values  # converter dataframe para matriz (lista com lista de cada registo)
@@ -71,7 +44,7 @@ def load_data(df_dados, janela):
     for i in range(len(mat_dados) - tam_sequencia):  # numero de registos - tamanho da sequencia
         res.append(mat_dados[i: i + tam_sequencia])
     res = np.array(res)  # dá como resultado um np com uma lista de matrizes (janela deslizante ao longo da serie)
-    qt_casos_treino = int(round(0.9 * res.shape[0]))  # 90% passam a ser casos de treino
+    qt_casos_treino = 24 # dois anos de treino, um de teste
     train = res[:qt_casos_treino, :]
     x_train = train[:, :-1]  # menos um registo pois o ultimo registo é o registo a seguir à janela
     y_train = train[:, -1][:, -1]  # para ir buscar o último atributo para a lista dos labels
@@ -82,17 +55,6 @@ def load_data(df_dados, janela):
     return [x_train, y_train, x_test, y_test]
 
 
-# Etapa 2 - Definir a topologia da rede (arquitectura do modelo) e compilar '''
-def build_model2(janela):
-    model = Sequential()
-    model.add(LSTM(128, input_shape=(janela, 3), return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(64, input_shape=(janela, 3), return_sequences=False))
-    # model.add(Dropout(0.2))
-    model.add(Dense(16, activation="relu", kernel_initializer="uniform"))
-    model.add(Dense(1, activation="linear", kernel_initializer="uniform"))
-    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
-    return model
 
 
 # imprime um grafico com os valores de teste e com as correspondentes tabela de previsões
@@ -117,20 +79,44 @@ def print_model(model, fich):
     plot_model(model, to_file=fich, show_shapes=True, show_layer_names=True)
 
 
-def LSTM_utilizando_GOOGL_data():
-    df = load_GOOGL_stock_dataset()
-    df = pre_processar_GOOGL_stock_dataset(df)
+
+
+# Etapa 2 - Definir a topologia da rede (arquitectura do modelo) e compilar
+def build_model2(janela):
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(1, 2))) ## TODO:ver melhor
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+    return model
+
+def load_ad_dataset():
+    return get_ad_data(0, 'adData.csv')
+
+
+def LSTM_utilizando_ad_data():
+    df = load_ad_dataset()
     print("df", df.shape)
-    janela = 22  # tamanho da Janela deslizante um mes, sem fim de semana por causa da bolsa
-    X_train, y_train, X_test, y_test = load_data(df[::-1], janela)  # o df[::-1] é o df por ordem inversa
+    janela = 1  # tamanho da Janela deslizante um ano
+    X_train, y_train, X_test, y_test = load_data(df, janela)
     print("X_train", X_train.shape)
     print("y_train", y_train.shape)
     print("X_test", X_test.shape)
     print("y_test", y_test.shape)
-    # model = build_model(janela)
+
+
+if __name__ == '__main__':
+    # visualize_GOOGL()
+    df = load_ad_dataset()
+    print("df", df.shape)
+    janela = 1  # tamanho da Janela deslizante um ano
+    X_train, y_train, X_test, y_test = load_data(df, janela)
+    print("X_train", X_train.shape)
+    print("y_train", y_train.shape)
+    print("X_test", X_test.shape)
+    print("y_test", y_test.shape)
     model = build_model2(janela)
-    # model.fit(X_train, y_train, batch_size=512, epochs=500, validation_split=0.1, verbose=1)
-    model.fit(X_train, y_train, batch_size=512, epochs=1, validation_split=0.1, verbose=1)
+    model.fit(X_train, y_train, batch_size=10, epochs=1, validation_split=0.1, verbose=1)
     print_model(model, "lstm_model.png")
     trainScore = model.evaluate(X_train, y_train, verbose=0)
     print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
@@ -143,6 +129,3 @@ def LSTM_utilizando_GOOGL_data():
     print_series_prediction(y_test, predic)
 
 
-if __name__ == '__main__':
-    # visualize_GOOGL()
-    LSTM_utilizando_GOOGL_data()
